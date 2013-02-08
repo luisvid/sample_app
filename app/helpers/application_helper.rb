4da833f66e44210aee8a10c2b350a495291908ee
LUIS VIDELA <luisvid@gmail.com>
2013-02-08 11:03:16 -0300

    5.5 exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
     end
   end
 
+  #Return the logo to be used in the partial: _header.html.erb
+  #<% logo = image_tag("logo.png", :alt => "Sample App", :class => "round") %>
+  def logo
+  	image_tag "logo.png", :alt => "Sample App", :class => "round"
+  end
+
+
 end
